afc8b1ef9dc6671e385c3bda0a01ee722e7d9975
guillermooo <guilan70@hotmail.com>
2011-09-23 23:40:14 +0200

    Improve ExWriteFile command.

--- ex_commands.py
+++ ex_commands.py
@@ -9,7 +9,16 @@ class ExPrintWorkingDir(sublime_plugin.TextCommand):
 
 
 class ExWriteFile(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, edit, file_name='', range=None, **kwargs):
+        if file_name and file_name != os.path.basename(self.view.file_name()):
+            v = self.view.window().new_file()
+            v.set_name(file_name)
+            return
+        
+        if range:
+            sublime.status_message('VintageEx: Command not implemented.')
+            return
+
         self.view.run_command('save')
 
 
