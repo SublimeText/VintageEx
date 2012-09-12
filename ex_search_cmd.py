189d522684b01f9d77f22a26b756bf0957d121df
guillermooo <guillermo.lopez@outlook.com>
2012-09-12 23:36:03 +0200

    ï»¿# HG changeset patch # User guillermooo <guillermo.lopez@outlook.com> # Date 1347485535 -7200 # Node ID 12778d262a2de321a3adcd16c3543f65fc518928 # Parent  6dd4582dcd755aa899aa18176d9c724c4db0346a implement realtime search feedback

--- ex_search_cmd.py
+++ ex_search_cmd.py
@@ -10,7 +10,9 @@ import ex_location
 
 class SearchImpl(object):
     last_term = ""
-    def __init__(self, view, cmd):
+    def __init__(self, view, cmd, remember=True, start_sel=None):
+        self.start_sel = start_sel
+        self.remember = remember
         if not cmd:
             return
         self.view = view
@@ -26,8 +28,9 @@ class SearchImpl(object):
     def search(self):
         if not getattr(self, "cmd", None):
             return
-        SearchImpl.last_term = self.cmd
-        sel = self.view.sel()[0]
+        if self.remember:
+            SearchImpl.last_term = self.cmd
+        sel = self.start_sel[0]
 
         next_match = None
         if self.reversed:
@@ -45,7 +48,10 @@ class SearchImpl(object):
             next_match = self.view.find(self.cmd, sel.end())
         if next_match:
             self.view.sel().clear()
-            self.view.sel().add(next_match)
+            if not self.remember:
+                self.view.add_regions("vi_search", [next_match], "search.vi", sublime.DRAW_OUTLINED)
+            else:
+                self.view.sel().add(next_match)
             self.view.show(next_match)
         else:
             sublime.status_message("VintageEx: Could not find:" + self.cmd)
@@ -53,17 +59,43 @@ class SearchImpl(object):
 
 class ViRepeatSearchBackward(sublime_plugin.TextCommand):
    def run(self, edit):
-       SearchImpl(self.view, "?" + SearchImpl.last_term).search()
+       SearchImpl(self.view, "?" + SearchImpl.last_term, start_sel=self.view.sel()).search()
 
 
 class ViRepeatSearchForward(sublime_plugin.TextCommand):
     def run(self, edit):
-        SearchImpl(self.view, SearchImpl.last_term).search()
+        SearchImpl(self.view, SearchImpl.last_term, start_sel=self.view.sel()).search()
 
 
 class ViSearch(sublime_plugin.TextCommand):
-   def run(self, edit, initial_text=""):
-       self.view.window().show_input_panel("", initial_text, self.on_done, None, None)
+    def run(self, edit, initial_text=""):
+        self.original_sel = list(self.view.sel())
+        self.view.window().show_input_panel("", initial_text,
+                                            self.on_done,
+                                            self.on_change,
+                                            self.on_cancel)
 
-   def on_done(self, s):
-        SearchImpl(self.view, s).search()
+    def on_done(self, s):
+        self._restore_sel()
+        SearchImpl(self.view, s, start_sel=self.original_sel).search()
+        self.original_sel = None
+        self._restore_sel()
+
+    def on_change(self, s):
+        if s in ("/", "?"):
+            return
+        self._restore_sel()
+        SearchImpl(self.view, s, remember=False, start_sel=self.original_sel).search()
+
+    def on_cancel(self):
+        self._restore_sel()
+        self.original_sel = None
+
+    def _restore_sel(self):
+        self.view.erase_regions("vi_search")
+        if not self.original_sel:
+            return
+        self.view.sel().clear()
+        for s in self.original_sel:
+            self.view.sel().add(s)
+        self.view.show(self.view.sel()[0])
